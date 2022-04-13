794fa33269a6d186be0a8515a66e199c47e39a72
Sunny Yang <guang.yang@thoughtworks.com>
2022-04-14 00:47:39 +0800

    build(#1): introduce next-ui

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import { CssBaseline } from '@nextui-org/react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [<>{initialProps.styles}</>],
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
