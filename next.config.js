f681e4ec9c03aed3f28047d1697c5bf9aab255ef
Sunny Yang <guang.yang@thoughtworks.com>
2022-04-15 02:11:30 +0800

    feat(#1): configure environments to use different servers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  const commonConfig = {
+    reactStrictMode: true,
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...commonConfig,
+      rewrites: async () => {
+        return [
+          {
+            source: '/:path*',
+            destination: process.env.SERVER_HOST + '/:path*',
+          },
+        ];
+      },
+    };
+  }
+
+  return commonConfig;
+};
