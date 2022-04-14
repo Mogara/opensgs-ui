e5985cb325e9c9c06bdd1cfbd229ec07ad830549
Sunny Yang <guang.yang@thoughtworks.com>
2022-04-15 02:26:24 +0800

    test(#1): move __tests__ to better location

--- pages/__tests__/index.test.tsx
+++ pages/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import Home from '../pages/index';
+import Home from '../index';
 import '@testing-library/jest-dom';
 
 describe('Home', () => {
